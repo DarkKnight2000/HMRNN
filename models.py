2fc2de63bb8c0f6f89bc4c9e688576deaa39d83b
Rishi <rishikaley@gmail.com>
2020-05-01 03:10:15 +0530

    Added new model

--- models.py
+++ models.py
@@ -5,6 +5,8 @@ from torch.nn.utils.rnn import pad_packed_sequence, pack_padded_sequence
 from collections import defaultdict
 
 
+use_cuda = False
+
 class SingleLSTMModel(nn.Module):
     '''
     input -> lat, long, timediff, cityid
@@ -23,24 +25,35 @@ class SingleLSTMModel(nn.Module):
         self.mseloss = nn.MSELoss()
         self.crossEntLoss = nn.CrossEntropyLoss()
 
+        if use_cuda : self.cuda_dev = torch.device('cuda')     # Default CUDA device
+
     def forward(self, x, hidden):
         x = x.view(self.batch_size, -1, self.input_size)
         # print(hidden.size())
         # Input : (batch, seqlen, input_size)
+        self.rnn.flatten_parameters()
         out, hidden = self.rnn(x, hidden)
         out = out.view(-1, self.hidden_size)
+        # if use_cuda: del h2
         return hidden, out
 
     def init_hidden(self):
-        return (Variable(torch.zeros(self.num_layers, self.batch_size, self.hidden_size)), Variable(torch.zeros(self.num_layers, self.batch_size, self.hidden_size)))
+        if use_cuda:
+            return (Variable(torch.zeros(self.num_layers, self.batch_size, self.hidden_size).cuda(device=self.cuda_dev)), Variable(torch.zeros(self.num_layers, self.batch_size, self.hidden_size).cuda(device=self.cuda_dev)))
+        else:
+            return (Variable(torch.zeros(self.num_layers, self.batch_size, self.hidden_size)), Variable(torch.zeros(self.num_layers, self.batch_size, self.hidden_size)))
 
 
     def CustomLoss(self, outputs, labels):
         output_dim = self.hidden_size - self.city_size
-        cur_loss = self.mseloss(outputs[0][:output_dim], torch.tensor(labels[:-1], dtype=torch.float))
-        return torch.add(cur_loss ,self.crossEntLoss(outputs[:, output_dim:], torch.tensor([labels[-1]], dtype=torch.long)))
+        cur_loss = self.mseloss(outputs[0][:output_dim], torch.tensor(labels[:-1], dtype=torch.float).cuda(device=self.cuda_dev))
+        return torch.add(cur_loss ,self.crossEntLoss(outputs[:, output_dim:], torch.tensor([labels[-1]], dtype=torch.long).cuda(device=self.cuda_dev)))
 
     def CustomTrain(self, df, inputs, labels, epochs):
+
+        f = open('logs.txt', 'a')
+
+        # if use_cuda: self.cuda(device=self.cuda_dev)
 
         optimiser = torch.optim.Adam(self.parameters(), lr = 0.05)
 
@@ -54,32 +67,39 @@ class SingleLSTMModel(nn.Module):
                 for inp, label in zip(inps, outs):
                     # print(hidden)
                     # print(torch.Tensor(inp).view(batch_size, -1, input_size))
-                    hidden, output = self(torch.Tensor(inp), hidden)
+                    if use_cuda:
+                        inp2 = torch.Tensor(inp).cuda(device=self.cuda_dev)
+                        label2 = torch.Tensor(label).cuda(device=self.cuda_dev)
+                    else:
+                        inp2 = torch.Tensor(inp)
+                    hidden, output = self(inp2, hidden)
                     # print(output)
                     # print(output.size())
                     # print('labe',label)
                     # print(label.size())
                     # try:
-                    loss += self.CustomLoss(output, label)
+                    del inp2
+                    loss += self.CustomLoss(output, label2)
                     # except AttributeError:
                     #     print(label)
-            if not epoch % 1 : print('\nepoch : ', epoch, ' loss: ', loss)
+            if not epoch % 1 : print('\nepoch : ', epoch, ' loss: ', loss, file=f)
 
             loss.backward()
             optimiser.step()
 
+        f.close()
 
 
 
+'''
 class MultiLSTMModel(nn.Module):
-    '''
-    For poi layer
-    input -> lat, long, timediff, cityid
-    output -> lat, long, timediff, binary switch(indicating if visits in current city are over)
-    For city layer
-    input -> curr city id
-    output vector -> next city id one hot encoded
-    '''
+
+    # For poi layer
+    # input -> lat, long, timediff, cityid
+    # output -> lat, long, timediff, binary switch(indicating if visits in current city are over)
+    # For city layer
+    # input -> curr city id
+    # output vector -> next city id one hot encoded
 
     def __init__(self, inp_size, hid_size, city_size, num_layers = [3,3], batch_size = 1):
         super(MultiLSTMModel, self).__init__()
@@ -132,10 +152,13 @@ class MultiLSTMModel(nn.Module):
         f = open('logs.txt', 'a')
 
         # training loop
+        print("length of inputs " + str(len(inputs)))
         for epoch in range(epochs):
+            print("epoch started " + str(epoch))
             optimiser.zero_grad()
             loss = 0
             for i in range(len(inputs)):
+                print("i done " + str(i))
                 inps, outs = inputs[i], labels[i] # for each user
                 hidden = [self.init_hidden_poi(), self.init_hidden_city()]
                 city_state[inputs[i][0][3]] = hidden[0]
@@ -162,3 +185,92 @@ class MultiLSTMModel(nn.Module):
 
             loss.backward()
             optimiser.step()
+'''
+
+
+
+class MultiLSTMModel(nn.Module):
+
+    def __init__(self, num_cities, poi_vec_len, city_rep_len = 128):
+        super(MultiLSTMModel, self).__init__()
+        self.num_cities = num_cities
+        self.poi_vec_len = poi_vec_len
+        self.city_rep_len = city_rep_len
+        self.inner_hidden_size = 1 + poi_vec_len
+        self.innerLstm = None
+        self.innerLstmDict = defaultdict(lambda : nn.LSTM(input_size = city_rep_len, hidden_size = self.inner_hidden_size, batch_first = True, num_layers = 1))
+        self.innerHidDict = defaultdict(lambda : (Variable(torch.zeros(1, 1, self.inner_hidden_size)), Variable(torch.zeros(1, 1, self.inner_hidden_size))))
+        self.outerLstm = nn.LSTM(input_size = num_cities, hidden_size = city_rep_len, batch_first = True, num_layers = 1)
+
+        self.mseloss = nn.MSELoss()
+        self.crossEntLoss = nn.CrossEntropyLoss()
+
+    def changeCity(self, city_id):
+        self.innerLstm = self.innerLstmDict[city_id]
+
+    def init_hidden_outer(self):
+        return (Variable(torch.zeros(1, 1, self.city_rep_len)), Variable(torch.zeros(1, 1, self.city_rep_len)))
+
+    def checkInLoss(self, output, label):
+        output = torch.split(output, self.poi_vec_len, dim=2)
+        return torch.add(self.mseloss(output[1], torch.tensor([[[label[1]]]], dtype=torch.float)) ,self.crossEntLoss(output[0].view(-1, self.poi_vec_len), torch.tensor([label[0]], dtype=torch.long)))
+
+def getEncodedVec(vec_len, on_at):
+    ret = torch.zeros(vec_len, dtype = torch.float)
+    print('ret', ret, 'onat ', on_at)
+    ret[on_at] = 1
+    if use_cuda : ret = ret.cuda(torch.device('cuda'))
+    return ret
+
+def MultiLstmTrain(model:MultiLSTMModel, data, epochs):
+
+    if use_cuda : model = model.cuda(torch.device('cuda'))
+
+    # optimiser_outer = torch.optim.Adam(model.outerLstm.parameters(), lr = 0.05)
+
+    # to store no of visits by a user in each city
+    user_city_visits = defaultdict(lambda : defaultdict(lambda : 0))
+
+    f = open('logs.txt', 'a')
+
+    # training loop
+    for epoch in range(epochs):
+        print("epoch started " + str(epoch))
+        total_loss = 0
+        # for each user
+        print(data[0][0])
+        for u_visit in data:
+            # optimiser_outer.zero_grad()
+            # outer_loss = 0
+            outer_hidden = model.init_hidden_outer()
+            # for each city a user visited
+            for c_visit in u_visit:
+                # print('cvisit1',c_visit[1])
+                curr_city = c_visit[0]
+                # print('curcity', curr_city)
+                inp_inner, outer_hidden = model.outerLstm(getEncodedVec(model.num_cities, curr_city).view(1, 1, -1), outer_hidden)
+                
+
+                model.changeCity(curr_city)
+                hidden_inner = model.innerHidDict[curr_city]
+                innerLoss = 0
+                optimiser_inner = torch.optim.Adam(model.innerLstm.parameters(), lr = 0.05)
+                optimiser_inner.zero_grad()
+                for cin in c_visit[1]:
+                    pred_checkin, hidden_inner = model.innerLstm(inp_inner, hidden_inner)
+                    innerLoss += model.checkInLoss(pred_checkin, cin)
+
+                total_loss += innerLoss
+                innerLoss.backward(retain_graph=True)
+                optimiser_inner.step()
+                del optimiser_inner
+                model.innerHidDict[curr_city] = hidden_inner
+
+            # outer_loss.backward()
+            # optimiser_outer.step()
+
+            # deleting hidden states of every city's lstm and starting newly for each user
+            for k in model.innerHidDict.keys():
+                del model.innerHidDict[k]
+
+        if not epoch % 1 : print('\nepoch : ', epoch, ' loss: ', total_loss, file = f)
--- training.py
+++ training.py
@@ -5,8 +5,9 @@ from torch.nn.utils.rnn import pad_packed_sequence, pack_padded_sequence
 import random
 import pandas as pd
 import numpy as np
+import pickle
 
-from models import SingleLSTMModel, MultiLSTMModel
+from models import SingleLSTMModel, MultiLSTMModel, MultiLstmTrain
 
 random.seed(0)
 np.random.seed(0)
@@ -26,13 +27,12 @@ seq_len = 1
 num_layers = 2
 epochs = 100
 
-f = open('logs.txt', 'w')
 
 def getData(df):
     inputs, labels, lastCheckins = [], [], []
     for i,uid in enumerate(df[user_id_col].unique()):
         sub_df = df[df[user_id_col] == uid]
-        if not i % 50: print('usr id', uid, ' dflen ', len(sub_df.index), file = f)
+        if not i % 50: print('usr id', uid, ' dflen ', len(sub_df.index))
         # inp = lat, long, time, cid
         # out = lat, long, time, binary, cid
         inps = np.array((sub_df[lat_col].to_list(), sub_df[long_col].to_list(), sub_df[time_col].to_list(), sub_df[city_id_col].to_list())).T/np.array([90,180,1,1])# normalising
@@ -52,31 +52,82 @@ def getData(df):
     return inputs, labels, lastCheckins
 
 
-def getEncodedVec(input, size):
-    # Last value of input should be city id
-    # size is total no of cities
-    retVal = torch.cat((input[:-1], torch.zeros(size)), 0)
-    print('revalsize', retVal.size())
-    print('cityid', input[-1])
-    retVal[input.size()[0] - 1 + int(input[-1])] = 1
-    return retVal
+def getSessionData(df):
+
+    # assigning venue id with respect to each city
+    city_count = df[city_id_col].max()
+    df2 = [set() for _ in range(city_count+1)]
+    max_venues = 0
+    for ind in df.index:
+        df2[df[city_id_col][ind]].add(df[poi_id_col][ind])
+        max_venues = max_venues if max_venues > len(df2[df[city_id_col][ind]]) else len(df2[df[city_id_col][ind]])
+    venue_id_map = dict()
+    for i in range(len(df2)):
+        df2[i] = list(df2[i])
+        for j in range(len(df2[i])):
+            venue_id_map[df2[i][j]] = j # map from venue_id to its index in list of all venue_ids in current city
+    print('done venueids')
+    data = []
+    last_user_ind = -1
+    for ind in df.index:
+        if df[user_id_col][ind] != last_user_ind:
+            data.append([[df[city_id_col][ind], []]])
+            last_user_ind = df[user_id_col][ind]
+        if data[-1][-1][0] == df[city_id_col][ind]:
+            data[-1][-1][1].append([venue_id_map[df[poi_id_col][ind]], df[time_col][ind]])
+        else:
+            data[-1].append([df[city_id_col][ind], []])
+            data[-1][-1][1].append([venue_id_map[df[poi_id_col][ind]], df[time_col][ind]])
+
+    print('onecheckinentry', data[0][0])
+
+    # data[user_id] = [[city_id, list of checkins info in that city]]
+    return data, max_venues
 
 
-# reading dataset
+
+
+# def getEncodedVec(input, size):
+#     # Last value of input should be city id
+#     # size is total no of cities
+#     retVal = torch.cat((input[:-1], torch.zeros(size)), 0)
+#     print('revalsize', retVal.size())
+#     print('cityid', input[-1])
+#     retVal[input.size()[0] - 1 + int(input[-1])] = 1
+#     return retVal
+
+
+# Writing curr time in log file
+from datetime import datetime
+f = open('logs.txt', 'a')
+f.write('Time stamp : ' + str(datetime.now()) + '\n')
+
+load_data = True
 df = pd.read_csv('./Datasets/dataset_TIST2015/smalldata_final.csv')
-print(df.dtypes, file = f)
-print(len(df.index), file = f)
-inputs, labels, lasts = getData(df)
+
+if not load_data:
+    # reading dataset
+    # print(df.dtypes, file = f)
+    print('Total checkins : ', len(df.index), file = f)
+    # inputs, labels, lasts = getData(df)
+    data = getSessionData(df)
+    f = open("./DataPickles/session_data.pkl", "wb")
+    pickle.dump(data, f)
+    f.close()
+else:
+    f = open("./DataPickles/session_data.pkl", "rb")
+    data = pickle.load(f)
+    f.close()
 
 '''
 1 -> single lstm model
 2 -> multi lstm model
 '''
-model_name = 3
+model_name = 2
 
 if model_name == 1:
 
-    print('cities ', len(df['city_name'].unique()), file = f)
+    print('cities ', len(df['city_name'].unique()))
     city_vec_len = len(df['city_name'].unique())
     input_dim = 4
     # dim without city id
@@ -86,12 +137,13 @@ if model_name == 1:
     model.CustomTrain(df, inputs, labels, epochs)
 
 elif model_name == 2:
-    print('cities ', len(df['city_name'].unique()), file = f)
+    print('cities ', len(df['city_name'].unique()))
     city_vec_len = len(df['city_name'].unique())
-    input_dim = 4
-    output_dim = 4
 
 
     # creating model
-    model = MultiLSTMModel(input_dim, output_dim, city_vec_len, num_layers=[num_layers, num_layers], batch_size=batch_size)
-    model.CustomTrain(df, inputs, labels, epochs)
+    model = MultiLSTMModel(city_vec_len, data[1])
+    MultiLstmTrain(model, data[0], 100)
+
+
+f.close()
